25cdda5e4bec31ee1588a73725b0ef6bab008fa6
woocastle <dldntjd3@naver.com>
2022-11-30 17:53:24 +0900

    2022-11-30 팀프로젝트 3일차

---
+++ island_team_project/island_team_modeling.py
@@ -0,0 +1,36 @@
+# modeling
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import *
+from keras.layers import *
+
+X_train, X_test, Y_train, Y_test = np.load(
+    './yogiyo_data_max_4_wordsize_4981.npy', allow_pickle=True)
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
+
+model = Sequential()
+model.add(Embedding(4981, 300, input_length=4))                 #5592차원을 100차원으로 줄이는 것
+model.add(Conv1D(32, kernel_size=5, padding='same', activation='relu'))
+model.add(MaxPool1D(pool_size=1))           # pool_size는 변화 없으므로 안써줘도 되지만 관습적으로 써놓자
+model.add(GRU(128, activation='tanh', return_sequences=True))           # return_sequences=True 를 주면 계산해서 나오는 값들을 저장해서 sequences 형태로 넘겨준다.
+model.add(Dropout(0.3))
+model.add(GRU(64, activation='tanh', return_sequences=True))
+model.add(Dropout(0.3))
+model.add(GRU(64, activation='tanh'))
+model.add(Dropout(0.3))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(11, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam',
+              metrics=['accuracy'])
+fit_hist = model.fit(X_train, Y_train, batch_size=128,
+                     epochs=1000, validation_data=(X_train, Y_train))
+model.save('./yogiyo_category_classification_model_{}.h5'.format(
+    np.round(fit_hist.history['val_accuracy'][-1], 3)))
+plt.plot(fit_hist.history['accuracy'], label='accuracy')
+plt.plot(fit_hist.history['val_accuracy'], label='val_accuracy')
+plt.legend()
+plt.show()
---
+++ island_team_project/island_team_preprocessing.py
@@ -0,0 +1,51 @@
+# preprocessing
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras_preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+import pickle
+
+pd.set_option('display.unicode.east_asian_width', True)                 # 줄 맞춰주기
+df = pd.read_csv('./crawling_data.csv')
+print(df.head())
+print(df.category.value_counts())
+
+X = df['restaurant_name']
+Y = df['category']
+
+encoder = LabelEncoder()
+labeled_Y = encoder.fit_transform(Y)        # 카테고리를 숫자로 라벨링!
+print(labeled_Y[:5])
+print(encoder.classes_)     # 몇 번으로 라벨링 되어있는지 확인 할수 있다.(보통 오름차순으로 정렬을 해서 받는다)
+with open('./label_encoder.pickle', 'wb') as f:
+    pickle.dump(encoder, f)
+onehot_Y = to_categorical(labeled_Y)
+print(onehot_Y[:5])
+
+token = Tokenizer()     # 자연어 처리를 위해 라벨링, 문장으로부터 단어를 토큰화하고 숫자에 대응시키는 딕셔너리를 사용
+token.fit_on_texts(X)
+tokened_X = token.texts_to_sequences(X)
+wordsize = len(token.word_index) + 1
+with open('./yogiyo_token.pickle', 'wb') as f:
+    pickle.dump(token, f)
+
+# 문장 길이 맞추기!
+max_len = 0
+for i in range(len(tokened_X)):
+    if max_len < len(tokened_X[i]):
+        max_len = len(tokened_X[i])
+print(max_len)
+
+X_pad = pad_sequences(tokened_X, max_len)
+
+# 테스트 데이터 나누기
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X_pad, onehot_Y, test_size=0.3)
+print(X_train.shape, Y_train.shape, X_test.shape, Y_test.shape)
+
+# 데이터 저장히기
+xy = X_train, X_test, Y_train, Y_test
+np.save('./yogiyo_data_max_{}_wordsize_{}.npy'.format(max_len, wordsize), xy)
